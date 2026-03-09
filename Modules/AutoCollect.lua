78164d8dbc1e2bfaf8b94961aa53bc72fd828033
kdbull96 <kdbull96@gmail.com>
2026-03-10 05:50:46 +0700

    Add AutoCollect module for collecting toothpaste
    Implement auto-collect feature for toothpaste items in the game.

--- Modules/AutoCollect.lua
+++ Modules/AutoCollect.lua
@@ -1,1 +1,39 @@
+-- AUTO COLLECT MODULE
 
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local root = char:WaitForChild("HumanoidRootPart")
+
+getgenv().AutoCollect = false
+
+task.spawn(function()
+	while true do
+		if getgenv().AutoCollect then
+			
+			for _,base in pairs(workspace.MAP_Game.Bases:GetChildren()) do
+				local toothpaste = base:FindFirstChild("Toothpaste")
+				
+				if toothpaste then
+					local slots = toothpaste:FindFirstChild("Slots")
+					
+					if slots then
+						for _,slot in pairs(slots:GetChildren()) do
+							
+							local collect = slot:FindFirstChild("Recolect")
+							
+							if collect and collect:IsA("BasePart") then
+								firetouchinterest(root, collect, 0)
+								firetouchinterest(root, collect, 1)
+							end
+							
+						end
+					end
+				end
+			end
+			
+		end
+		
+		task.wait(0.05)
+	end
+end)
