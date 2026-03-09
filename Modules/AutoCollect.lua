7dcb1d94dd72b48a9000fe5b1d50b25bee9556a6
kdbull96 <kdbull96@gmail.com>
2026-03-10 06:00:55 +0700

    Integrate KDBFeatures into AutoCollect module
    Refactor AutoCollect module to integrate KDBFeatures and improve structure.

--- Modules/AutoCollect.lua
+++ Modules/AutoCollect.lua
@@ -1,39 +1,60 @@
--- AUTO COLLECT MODULE
-
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
-local char = player.Character or player.CharacterAdded:Wait()
-local root = char:WaitForChild("HumanoidRootPart")
 
-getgenv().AutoCollect = false
+getgenv().KDBFeatures["Auto Collect"] = {
+Enabled = false
+}
+
+local feature = getgenv().KDBFeatures["Auto Collect"]
 
 task.spawn(function()
+
 	while true do
-		if getgenv().AutoCollect then
-			
-			for _,base in pairs(workspace.MAP_Game.Bases:GetChildren()) do
-				local toothpaste = base:FindFirstChild("Toothpaste")
+	
+		if feature.Enabled then
+		
+			local char = player.Character
+			if char then
 				
-				if toothpaste then
-					local slots = toothpaste:FindFirstChild("Slots")
+				local root = char:FindFirstChild("HumanoidRootPart")
+				
+				if root then
 					
-					if slots then
-						for _,slot in pairs(slots:GetChildren()) do
+					for _,base in pairs(workspace.MAP_Game.Bases:GetChildren()) do
+						
+						local toothpaste = base:FindFirstChild("Toothpaste")
+						
+						if toothpaste then
 							
-							local collect = slot:FindFirstChild("Recolect")
+							local slots = toothpaste:FindFirstChild("Slots")
 							
-							if collect and collect:IsA("BasePart") then
-								firetouchinterest(root, collect, 0)
-								firetouchinterest(root, collect, 1)
+							if slots then
+								
+								for _,slot in pairs(slots:GetChildren()) do
+									
+									local collect = slot:FindFirstChild("Recolect")
+									
+									if collect then
+										firetouchinterest(root, collect, 0)
+										firetouchinterest(root, collect, 1)
+									end
+									
+								end
+								
 							end
 							
 						end
+						
 					end
+					
 				end
+				
 			end
 			
 		end
 		
 		task.wait(0.05)
+		
 	end
+
 end)
