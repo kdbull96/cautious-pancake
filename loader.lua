f53b9c64169c23ad23479a95a50abec390d34250
kdbull96 <kdbull96@gmail.com>
2026-04-06 00:50:04 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,14 @@
+local placeId = game.PlaceId
+
+local games = {
+    [1775411365171] = "UpgradeALuckyBlock"
+}
+
+local gameName = games[placeId]
+
+if gameName then
+    local url = "https://raw.githubusercontent.com/kdbull96/cautious-pancake/main/Games/" .. gameName .. ".lua?v=" .. tick()
+    loadstring(game:HttpGet(url))()
+else
+    warn("Game not supported")
+end
